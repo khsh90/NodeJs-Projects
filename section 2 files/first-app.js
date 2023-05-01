37ce1eeb03f02c5620277ab6d7cccbcd9542b30e
Khaled Hadidi <khsh400@gmail.com>
2023-05-01 16:14:32 +0300

    finish section 2

---
+++ section 2 files/first-app.js
@@ -0,0 +1,2 @@
+const fs=require('fs');
+fs.writeFileSync('hello.txt','This the first message from nodejs',);
---
+++ section 2 files/play.js
@@ -0,0 +1,44 @@
+const personD=(personData)=>{
+    console.log(personData.name)
+}
+
+
+
+const person2={
+    name:"kk"
+}
+
+
+// const person={
+//     name: 'John',
+//     age: 30,
+//     great () {console.log('hi am '+this.name)}
+// }
+
+const {name,age}=person;
+
+console.log(age,name)
+
+
+// console.log(person);
+// person.great();
+
+
+// const kk=[1,2,3]
+
+// const copykk=[...kk,4];
+
+
+// console.log(copykk);
+
+
+// const toArray=(...arg)=> arg;
+
+
+// console.log(toArray(1,2,3,4));
+
+
+
+
+
+personD(person);
