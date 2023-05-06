386f37e688c3b33d1a573edb83b07f9d4470ea07
Khaled Hadidi <khsh400@gmail.com>
2023-05-06 11:50:58 +0300

    finish section 6 with clean up

--- section 6 files/app.js
+++ section 6 files/app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const adminData = require("./routes/admin");
 const shopeRouter = require("./routes/shop");
 const path = require("path");
-//const expressHbs = require("express-handlebars");
 
 const app = express();
 
@@ -13,18 +12,10 @@ app.use(express.static(path.join(__dirname, "public"))); // in order to access t
 app.use("/admin", adminData.router);
 app.use(shopeRouter);
 
-// app.engine("hbs",expressHbs({
-//   layoutsDir: 'views/layouts/',
-//   defaultLayout: 'main-layout',
-//   extname: 'hbs'
-// }));
-app.set('view engine','ejs')
-//app.set("view engine", "hbs");
-//app.set('view engine','pug')
+app.set("view engine", "ejs");
 
 app.set("views", "views"); //the first view is reserved , the second view the point to views folder
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
   res.status(404).render("404", { pageTitle: "page not found" }); //both hbs and pug use the same syntax of page title
 });
 
--- section 6 files/routes/admin.js
+++ section 6 files/routes/admin.js
@@ -4,18 +4,11 @@ const router = express.Router();
 
 const product = [];
 
-//admin/add-product=>with get
-//dir name mean the dirctory of files which here means routes , we bput ../ to get out one level
 router.get("/add-product", (req, res, next) => {
-  // res.sendFile(path.join(__dirname,'..','views','add-product.html'))
   res.render("add-product", {
     pageTitle: "add-product",
     path: "/admin/add-product",
     // the path will be used for making the add-product active that used in add-product.ejs
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,  
-    // the above three used for handle bars.
   });
 });
 
@@ -23,7 +16,7 @@ router.get("/add-product", (req, res, next) => {
 router.post("/add-product", (req, res, next) => {
   res.redirect("/");
 
-  console.log(req.body);
+ // console.log(req.body);
 
   product.push({ title: req.body.title });
 });
